a3c4fcbf9c3d330c0ac08cee0c2504045862022b
rcontrisha <rcontrisha@gmail.com>
2026-09-07 13:23:03 +0700

    feat: create function to calculate area and perimeter of a rectangle

--- main.go
+++ main.go
@@ -4,4 +4,23 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World")
+	countRectangle(10, 5)
+}
+
+func countPerimeter (width uint8, height uint8) uint8 {
+	return 2 * (width + height)
+}
+
+func countArea (width uint8, height uint8) uint8 {
+	return width * height
+}
+
+func countRectangle (width uint8, height uint8) {
+	var perimeter uint8 = countPerimeter(width, height)
+	var area uint8 = countArea(width, height)
+
+	fmt.Println("\nCount Area & Perimeter of Rectangle")
+	fmt.Println("=======================================")
+	fmt.Printf("Area: %d\n", area)
+	fmt.Printf("Perimeter: %d", perimeter)
 }
